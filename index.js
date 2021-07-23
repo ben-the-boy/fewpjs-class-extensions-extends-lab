d947b5d83cc241b98ad8041d0d83328e1d87319c
Ben Hayes <b.t.hayes@comcast.net>
2021-07-23 13:52:12 -0500

    commit tree ca6837a353886769e9f4446418d6bcabf9b93898

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Polygon {
+  constructor(sideLengths) {
+    this.sideLengths = sideLengths;
+  }
+
+  get countSides() {
+    return this.sideLengths.length;
+  }
+
+  get perimeter() {
+    return this.sideLengths.reduce(function(memo, i) {
+      return memo + i;
+    })
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    return (this.sideLengths[0] + this.sideLengths[1] > this.sideLengths[2]) &&
+    (this.sideLengths[1] + this.sideLengths[2] > this.sideLengths[0]) &&
+    (this.sideLengths[0] + this.sideLengths[2] > this.sideLengths[1])
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return (this.sideLengths[0] === this.sideLengths[1]) && (this.sideLengths[1] ===
+    this.sideLengths[2]) && (this.sideLengths[2] === this.sideLengths[3])
+  }
+
+  get area() {
+    return this.sideLengths[0] ** 2
+  }
+}
